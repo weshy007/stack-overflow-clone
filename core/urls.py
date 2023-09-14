3ce296a88518ba043a9ec2c6faf9a685e7dca678
Weshy Joe <josephwaweru96@gmail.com>
2023-09-14 07:57:06 +0300

    templates and views

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_view
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Authentication System
+    path('register/', user_view.register, name="register"),
+    path('login/', auth_view.LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name="logout.html"), name='logout'),
+
+    # Profile system
+    path('profile/', user_view.profile, name="profile"),
+    path('profile/update/', user_view.profile_update, name="profile_update")
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from users.forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            form.save()
+            messages.success(request, f'Account Successfully Created. You can Login!')
+            return redirect('login')
+
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+@login_required()
+def profile_update(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account Updated Successfully')
+            return redirect('profile')
+
+        else:
+            u_form = UserUpdateForm(request.POST, instance=request.user)
+            p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        context = {
+            'u_form': u_form,
+            'p_form': p_form
+        }
+
+        return render(request, 'profile_update.html', context)
